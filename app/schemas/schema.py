51125c5def4102fc8721cd68811064543673c5d5
raphael-ph <raphasilvaesilva15@gmail.com>
2025-08-16 15:47:04 -0300

    feat(schema): adding the RAGResponse schema for controlled generation

--- app/schemas/schema.py
+++ app/schemas/schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
 
+# --- Indexing components ---
 class Document(BaseModel):
     """Base class representing a Document chunk to be indexed in Elasticsearch"""
     document_id: str = Field(..., description="Unique id for the document")
@@ -11,3 +12,14 @@ class Document(BaseModel):
     embedding: Optional[List[float]] = Field(None, description="Vector embedding for semantic search")
     source_file: Optional[str] = Field(None, description="Original file path or identifier")
     page_number: Optional[int] = Field(None, description="Page number in the original document (if applicable)")
+
+
+# --- Retrieval coomponents ---
+class RAGReference(BaseModel):
+    """Base object for holding the references to a generated response"""
+    reference_title: str = Field(..., description="The title of the referenced chunk or document.")
+    reference_excerpt: List[str] = Field(..., description="The exact excert that was used to generate that specific part of the response.")
+
+class RAGResponse(BaseModel):
+    """Base model for structured RAG response generation"""
+    reference: List[RAGReference] = Field(..., description="A list of all excerpts and title references used to generate the response.")
